e263c6f71e204e47e9aaffdb1b3a937e9aa412e8
Victor <pobozhny94@gmail.com>
2024-03-16 18:06:26 +0300

    errors 3 types

--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,17 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { setAppStatus } from '../../app/app-reducer'
+import axios from 'axios'
+
+
+type ErrorType = {
+  'errorMessages': [
+    {
+      'field': string,
+      'message': string
+    }
+  ]
+}
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   dispatch(setAppStatus('loading'))
@@ -15,9 +26,16 @@ export const fetchDecksTC = () => async (dispatch: Dispatch) => {
 }
 
 export const addDeckTC = (name: string) => async (dispatch: Dispatch) => {
-  return decksAPI.addDeck(name).then((res) => {
+  try {
+    const res = await decksAPI.addDeck(name)
     dispatch(addDeckAC(res.data))
-  })
+  } catch (e) {
+    if (axios.isAxiosError<ErrorType>(e)) {
+      if (!!e.response) {
+        console.log('error1: ', e.response.data.errorMessages[0].message)
+      }
+    }
+  }
 }
 
 export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
@@ -27,7 +45,17 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
 }
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (e) {
+    let errorMessage: string
+
+    if (axios.isAxiosError<ErrorType>(e)) {
+      errorMessage = e.response ? e.response.data.errorMessages[0].message : e.message
+    } else {
+      errorMessage = e.message
+    }
+    console.log(errorMessage)
+  }
 }
